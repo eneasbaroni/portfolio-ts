432102da62ebb2ebc90a5bad47bce20246137adb
Eneas <87401725+eneasbaroni@users.noreply.github.com>
2023-11-22 12:41:39 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,30 @@
 import { BrowserRouter, Route, Routes} from 'react-router-dom'; 
 import './App.css';
-
 import Footer from './components/Footer/Footer';
 import { LanguageProvider } from './context/languageContext';
-/* import Home from './Pages/Home/Home';
+import AppContainer from './components/AppContainer/AppContainer';
+import Navbar from './components/Navbar/Nabvar';
+import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Works from './Pages/Works/Works';
 import Contact from './Pages/Contact/Contact';
-import AppContainer from './components/AppContainer/AppContainer';
-import Footer from './components/Footer/Footer';
-import Navbar from './components/Navbar/Navbar'; */
 
-
-/* import "./grid.css" */
 
 function App() {  
   return (
-    <LanguageProvider>
-      <h1>HOLAAA</h1>
-      <BrowserRouter>
-
-      <Routes>
-        <Route path='/' element={<h1>Borrar</h1>}/>
-      </Routes>
-        {/* <AppContainer>      
+    <LanguageProvider>      
+      <BrowserRouter>      
+        <AppContainer>      
           <Navbar/>
           <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/about' element={<About/>}/>
-            <Route path='/works' element={<Works/>}/>
-            <Route path='/contact' element={<Contact/>}/>
+            <Route path='/works' element={<Works/>}/>   
+            <Route path='/contact' element={<Contact  />}/>    
           </Routes>
-        </AppContainer> */}
+          <Footer/>
+        </AppContainer>
       </BrowserRouter>      
-      <Footer/>
     </LanguageProvider>
     
   );
